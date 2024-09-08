f2afe78146ed7f228013e44e59883349b3e8717b
Pravin Kanotara <pkanotara5204@gmail.com>
2024-09-08 11:25:34 +0530

    login page

--- work/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('enrollBtn').addEventListener('mouseover', function() {
-    document.getElementById('enrollTooltip').style.display = 'block';
-});
-
-document.getElementById('enrollBtn').addEventListener('mouseout', function() {
-    document.getElementById('enrollTooltip').style.display = 'none';
-});
