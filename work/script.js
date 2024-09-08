3c673212ddeda4b3becc46dd9d2dd6034fd8a03b
Pravin Kanotara <pkanotara5204@gmail.com>
2024-09-08 11:08:58 +0530

    login page of internship work

---
+++ work/script.js
@@ -0,0 +1,7 @@
+document.getElementById('enrollBtn').addEventListener('mouseover', function() {
+    document.getElementById('enrollTooltip').style.display = 'block';
+});
+
+document.getElementById('enrollBtn').addEventListener('mouseout', function() {
+    document.getElementById('enrollTooltip').style.display = 'none';
+});
